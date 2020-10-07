878b4a93464cb56edc9b756f4c2f8f746f90fc13
Iury Piva <iurypiva@gmail.com>
2020-10-07 20:41:38 -0300

    move app to compontes

---
+++ src/components/AppBar/AppBar.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+import { AppBar as MuiAppBar, Container, Toolbar } from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+import logo from "assets/OFN_logo.svg";
+
+export const AppBar = () => (
+  <MuiAppBar position="static">
+    <Toolbar className="h-24 bg-gray-100 text-gray-700 ">
+      <Container className="flex px-24">
+        <Link to="/" className="flex-grow flex leading-4">
+          <img src={logo} className="App-logo" alt="logo" />
+          <span className="w-32 ml-4 pt-1/2">Open Food Network</span>
+        </Link>
+        <Link to="/cart" className="pt-1">
+          <ShoppingCart />
+          <span className="ml-2">My cart</span>
+        </Link>
+      </Container>
+    </Toolbar>
+  </MuiAppBar>
+);
